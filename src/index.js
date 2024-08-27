6bc80b01cb9deb1f5571fa355cb53c07a050eba5
Abdulmajeed Alselmi <hh738hhh@gmail.com>
2024-08-27 15:08:45 +0300

    fetch on demands with forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', init);
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querySelector("input#searchByID");
+
+    fetch("http://localhost:3000/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const movie = data.find((movie) => movie.id == input.value);
+        const title = document.querySelector("h4#movieDetailsTitle");
+        const summary = document.querySelector("section#movieDetailsSummary");
+
+        if (movie) {
+          title.innerText = movie.title;
+          summary.innerText = movie.summary;
+        } else {
+          title.innerText = "Movie not found";
+          summary.innerText = "";
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching movie data:", error);
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
